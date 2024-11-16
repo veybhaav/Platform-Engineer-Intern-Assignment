8faf062ae24a1ea5f20b4e4f49ee58d90b1bc004
Vaibhav Jaitwal <143623937+veybhaav@users.noreply.github.com>
2024-11-16 10:59:14 +0530

    delete main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,2 @@
-module "s3" {
-  source = "./modules/s3"
-  s3_bucket_name = var.s3_bucket_name
-}
+**This is not the part of the project.**
 
-module "sqs" {
-  source = "./modules/sqs"
-  queue_name = var.queue_name
-}
-
-module "secrets_manager" {
-  source = "./modules/secrets_manager"
-  secret_name = var.secret_name
-  secret_value = var.secret_value
-}
-
-module "ec2" {
-  source = "./modules/ec2"
-  instance_type = var.instance_type
-  ami_id = var.ami_id
-}
